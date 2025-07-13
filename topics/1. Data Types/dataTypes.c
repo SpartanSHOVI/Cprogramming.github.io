4e06eb4bcc09c74c09fa9e67ba4be53b8eebc85a
SpartanSHOVI <151990075+SpartanSHOVI@users.noreply.github.com>
2025-07-13 17:00:26 +0530

    Add files via upload

---
+++ topics/1. Data Types/dataTypes.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    int a = 1; // stores an integer
+    printf("%d\n", a);
+    
+    char c = 'A'; // stores a single character / symbol
+    printf("%c\n", c);
+
+    float b = 2.5; // stores floating value / decimal value
+    printf("%f\n", b);
+
+    double d = 3.141592653589793 ; // stores high-precision decimal numbers
+    printf("%lf\n", d );
+    printf(double pi);
+    return 0;
+}
