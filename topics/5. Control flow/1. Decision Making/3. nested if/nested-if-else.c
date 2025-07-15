6cd8ae62f5c36f6d9465ad780637ef3354376b29
SpartanSHOVI <151990075+SpartanSHOVI@users.noreply.github.com>
2025-07-15 18:52:07 +0530

    Add files via upload

---
+++ topics/5. Control flow/1. Decision Making/3. nested if/nested-if-else.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+    int i;
+
+    printf("Enter either 1 or 2");
+    scanf("%d", &i);
+
+    if( i == 1 ){
+        printf("You would go to heaven !");
+    }
+    else{
+        if( i == 2 ){
+            printf("Hell was created with you in mind");
+        }
+        else{
+            printf("How about mother earth !");
+        }
+    }
+    return 0;
+}
+
+/*
+
+if(condition) {
+    // stat
+} else {
+    if(condition) {
+        // stat
+    } else {
+        // stat 
+    }
+}
+
+*/
